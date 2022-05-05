a0e390e6213a267a566f946bb6b78b87102e4e26
Ekate <25ky-ky08@mail.ru>
2022-05-05 16:02:59 +0300

    'test4'

---
+++ 92.js
@@ -0,0 +1,23 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }, 
+    {
+      "artist": "dflkgd",
+      "title": "lklfk",
+      "release_year": 1989,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ]
+    }
+  ];
